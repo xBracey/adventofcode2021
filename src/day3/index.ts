aa78aa69c234a1ffa859e39f1000a49a51a1f603
Thomas Brace <tom@Thomass-Mac-mini.local>
2021-12-04 10:19:00 +0000

    Day 3 part 2:

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -28,5 +28,47 @@ export const day3 = async () => {
   const mostCommonDec = parseInt(mostCommonBits, 2);
   const leastCommonDec = Math.pow(2, rowLength) - 1 - mostCommonDec;
 
-  console.log(mostCommonDec * leastCommonDec);
+  // console.log(mostCommonDec * leastCommonDec);
+
+  const oxygen = await day3part2(true);
+  const co2 = await day3part2(false);
+
+  console.log(oxygen * co2);
+};
+
+// 1459
+
+export const day3part2 = async (oxygen: boolean) => {
+  let linesRaw: string[] = await readLines("./src/day3/data.txt");
+
+  let position = 0;
+
+  while (linesRaw.length > 1) {
+    let zeroCount = 0;
+    let oneCount = 0;
+
+    for (let index = 0; index < linesRaw.length; index++) {
+      const char = linesRaw[index][position];
+      if (char === "0") zeroCount++;
+      if (char === "1") oneCount++;
+    }
+
+    if (oxygen) {
+      if (zeroCount > oneCount) {
+        linesRaw = linesRaw.filter((l) => l[position] === "0");
+      } else {
+        linesRaw = linesRaw.filter((l) => l[position] === "1");
+      }
+    } else {
+      if (oneCount < zeroCount) {
+        linesRaw = linesRaw.filter((l) => l[position] === "1");
+      } else {
+        linesRaw = linesRaw.filter((l) => l[position] === "0");
+      }
+    }
+
+    position++;
+  }
+
+  return parseInt(linesRaw[0], 2);
 };
